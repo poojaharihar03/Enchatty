f4761671fc8b9797a1cb60e979d6b68cef266e78
POOJA HARIHAR <88924201+poojaharihar03@users.noreply.github.com>
2024-03-28 19:31:16 +0530

    implemented pdf+url chat
    additionally added the radio button 
    and 
    multiple models to choose from

--- pdfchat/app.py
+++ pdfchat/app.py
@@ -4,34 +4,14 @@ import tempfile
 import pdfplumber
 import streamlit as st
 from langchain.chains import RetrievalQA
-from langchain.text_splitter import CharacterTextSplitter
+from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import Chroma
 from langchain_community.llms import HuggingFaceHub
 from langchain_community.embeddings import HuggingFaceInferenceAPIEmbeddings
 from langchain_community.llms.huggingface_pipeline import HuggingFacePipeline
-from langchain_community.document_loaders import PDFPlumberLoader
+from langchain_community.document_loaders import PDFPlumberLoader, WebBaseLoader
 from langchain import hub
 from streamlit_chat import message as chat_message
-
-if "max_length" not in st.session_state:
-    st.session_state.max_length = 128  # Default value
-if "temp" not in st.session_state:
-    st.session_state.temp = 0.1  # Default value
-
-text_splitter = CharacterTextSplitter(
-    separator="\n",
-    chunk_size=1000,
-    chunk_overlap=200
-)
-pdf_file_path = None  # Initialize pdf_file_path
-
-def extract_data(feed):
-    data = []
-    with pdfplumber.load(feed) as pdf:
-        pages = pdf.pages
-        for p in pages:
-            data.append(p.extract_tables())
-    return None 
 
 # Function to record time
 time_start = 0
@@ -42,19 +22,33 @@ def record_timing():
         print(f"Time taken for query-response pair: {duration:.2f} seconds")
     time_start = time.time()
 
+# List of available LLM models
+llm_models = {
+    "Mistral-7B-Instruct-v0.2": "mistralai/Mistral-7B-Instruct-v0.2",
+    "Mixtral-8x7B-Instruct-v0.1": "mistralai/Mixtral-8x7B-Instruct-v0.1",
+    "Gemma-7B": "google/gemma-7b",
+    "Llama-2-7B (Gated Access)": "meta-llama/Llama-2-7b-chat-hf",
+    "Flan T5 Base": "google/flan-t5-base",
+    "GPT-2": "openai-community/gpt2"
+}
+
 # Frontend code
-st.title("Chat with PDF")
-st.write("**Upload your Hugging Face API token and PDF file below**", 
+st.title("Chat with PDF or URL")
+st.write("**Upload your Hugging Face API token and either upload a PDF file or enter a URL below**", 
          unsafe_allow_html=True, 
          format="markdown", 
-         style={'font-size': '30px'})
+         style={'font-size': '20px'})
 
 with st.sidebar:
-    st.markdown("<h1 style='text-align:center;font-family:Georgia;font-size:26px;'>🧑‍⚖️ Chat with PDF </h1>",
+    st.markdown("<h2 style='text-align:center;font-family:Georgia;font-size:20px;'>Choose Model</h2>",
                 unsafe_allow_html=True)
-    st.markdown("<h2 style='text-align:center;font-family:Georgia;font-size:20px;'>Features</h2>",
-                unsafe_allow_html=True)
-    st.markdown("Users can now add their own choice of PDFs and chat with our application")
+    
+    # Add a dropdown menu to select LLM model
+    selected_model = st.selectbox("Select LLM Model", list(llm_models.keys()))
+
+    # Display warning for models with gated access
+    if selected_model == "Llama-2-7B (Gated Access)":
+        st.warning("Access to this model requires authorization from Hugging Face.")
     
     # Add a div to contain the input field and label
     st.markdown("<div id='hf_token_input'></div>", unsafe_allow_html=True)
@@ -63,8 +57,8 @@ with st.sidebar:
     st.markdown("""
         <style>
             #hf_token_input {
-                position: fixed;
-                top: 10px; 
+                top: 5rem;
+                position: fixed; 
             }
         </style>
     """, unsafe_allow_html=True)
@@ -76,54 +70,69 @@ with st.sidebar:
     if not HF_token:
         st.error("Please enter your Hugging Face API token.")
     
+    # Allow user to choose between uploading PDF or entering URL
+    file_or_url = st.radio("Choose Input Type", ("PDF File", "URL"))
 
-    uploaded_file = st.file_uploader('Choose your .pdf file', type="pdf")
-    if uploaded_file is not None:
-    # Save the uploaded file to a temporary location
-        with tempfile.NamedTemporaryFile(delete=False) as tmp_file:
-            tmp_file.write(uploaded_file.getvalue())
-            pdf_file_path = tmp_file.name 
+    if file_or_url == "PDF File":
+        uploaded_file = st.file_uploader('Upload your .pdf file', type="pdf")
+        if uploaded_file is not None:
+            # Save the uploaded file to a temporary location
+            with tempfile.NamedTemporaryFile(delete=False) as tmp_file:
+                tmp_file.write(uploaded_file.getvalue())
+                content = PDFPlumberLoader(tmp_file.name).load()
+    else:  # Input type is URL
+        url = st.text_input("Enter the URL")
+        if url:
+            content = WebBaseLoader(url).load()
 
     st.markdown("<h2 style='text-align:center;font-family:Georgia;font-size:20px;'>Advanced Features</h1>",
                 unsafe_allow_html=True)
-    max_length = st.slider("Token Max Length", min_value=128, max_value=1024, value=st.session_state.max_length, step=128)
-    temp = st.slider("Temperature", min_value=0.1, max_value=1.0, value=st.session_state.temp, step=0.1)
+    max_length = st.slider("Token Max Length", min_value=128, max_value=1024, value=128, step=128)
+    temp = st.slider("Temperature", min_value=0.1, max_value=1.0, value=0.1, step=0.1)
     if st.button("Apply Settings"):
-        st.session_state.max_length = max_length
-        st.session_state.temp = temp
+        pass  # You can add your logic here if needed
 
-
-if pdf_file_path:
-    loader = PDFPlumberLoader(pdf_file_path)
-    pages = loader.load()
-    docs = text_splitter.split_documents(pages)
-    db = Chroma.from_documents(docs, HuggingFaceInferenceAPIEmbeddings(
-        api_key=HF_token, model_name="sentence-transformers/all-MiniLM-L6-v2"
-    ))
+if 'content' in locals():
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=512, chunk_overlap=50)
+    chunking = text_splitter.split_documents(content)
+    embeddings = HuggingFaceInferenceAPIEmbeddings(
+        api_key=HF_token,
+        model_name="sentence-transformers/all-MiniLM-L6-v2"
+    )
+    vectorstore = Chroma.from_documents(chunking, embeddings)
     prompt = hub.pull("rlm/rag-prompt", api_url="https://api.hub.langchain.com")
+
+    # Get the selected LLM model ID
+    selected_model_id = llm_models[selected_model]
 
     def model(user_query, max_length, temp):
         record_timing()  # Record time before generating response
-        repo_id = 'mistralai/Mixtral-8x7B-Instruct-v0.1'
         llm = HuggingFaceHub(
-            repo_id=repo_id,
+            repo_id=selected_model_id,
             huggingfacehub_api_token=HF_token,
-            model_kwargs={"max_length": 1024, "temperature": 0.1}
+            model_kwargs={"max_length": max_length, "temperature": temp}
         )
+        retriever = vectorstore.as_retriever(search_type="mmr", search_kwargs={"k": 2})
         qa = RetrievalQA.from_chain_type(llm=llm,
-                                         chain_type="stuff",
-                                         retriever=db.as_retriever(k=2),
-                                         return_source_documents=True,
-                                         verbose=True,
-                                         chain_type_kwargs={"prompt": prompt})
+                                          chain_type="stuff",
+                                          retriever=retriever,
+                                          return_source_documents=True,
+                                          verbose=True,
+                                          chain_type_kwargs={"prompt": prompt})
         response = qa(user_query)["result"]
-       
+
         answer_start = response.find("Answer:")
         if answer_start != -1:
             answer = response[answer_start + len("Answer:"):].strip()
             return answer
         else:
             return "Sorry, I couldn't find the answer."
+
+    # Reset chat when the model selection changes
+    if "selected_model" in st.session_state:
+        if st.session_state.selected_model != selected_model:
+            st.session_state.messages = []
+    st.session_state.selected_model = selected_model
 
     # CSS styling for the text input
     styl = f"""
@@ -153,18 +162,18 @@ if pdf_file_path:
         st.session_state.current_response = ""
 
     for message in st.session_state.messages:
-        chat_message(message["content"], is_user=message["role"] == "user")
+        chat_message(message["content"], is_user=message["role"] == "user", key=message["content"])  # Assign unique key
 
-    if user_prompt := st.text_input("Your message here", on_change=submit):
+    if user_prompt := st.text_input("Your message here", on_change=submit, key="text_input"):  # Assign unique key
 
         st.session_state.messages.append(
             {"role": "user", "content": user_prompt}
         )
-        chat_message(user_prompt, is_user=True)
-        response = model(user_prompt, st.session_state.max_length, st.session_state.temp)
+        chat_message(user_prompt, is_user=True, key=user_prompt)  # Assign unique key
+        response = model(user_prompt, max_length, temp)
         record_timing()  # Record time after generating response
 
         st.session_state.messages.append(
             {"role": "assistant", "content": response}
         )
-        chat_message(response)
+        chat_message(response, key=response)  # Assign unique key
